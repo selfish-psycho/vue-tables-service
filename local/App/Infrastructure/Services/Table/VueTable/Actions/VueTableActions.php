6ca4bf83ba85adbdb2c02110a7029aea3aedc074
Alexandr Knyazev <asknyazev2402@gmail.com>
2025-02-01 01:03:37 +0500

    Добавил в Actions подключение стилей и включение режима редактирования

--- local/App/Infrastructure/Services/Table/VueTable/Actions/VueTableActions.php
+++ local/App/Infrastructure/Services/Table/VueTable/Actions/VueTableActions.php
@@ -4,26 +4,81 @@ namespace App\Infrastructure\Services\Table\VueTable\Actions;
 
 use App\Infrastructure\Contracts\Tables\ActionsInterface;
 use App\Infrastructure\Contracts\Tables\DataClassInterface;
+use App\Infrastructure\Contracts\Tables\ServiceInterface;
 use App\Infrastructure\Enums\Table\ServicesEnums;
 use App\Infrastructure\Services\Table\TableService;
+use Bitrix\Main\Loader;
+use RuntimeException;
 
 class VueTableActions implements ActionsInterface
 {
+    private ServiceInterface $service;
+
     /**
      * Метод определяет DOM-узел для инициализации Vue-приложения и сохраняет класс описания сбора данных для таблицы
      * @param string $appId
      * @param DataClassInterface $dataClass
-     * @return void
+     * @return self
      */
-    public function init(string $appId, DataClassInterface $dataClass): void
+    public function init(string $appId, DataClassInterface $dataClass): self
     {
         //Определяем Vue-приложение
-        $service = TableService::create(ServicesEnums::VUE->value);
-        $service->repository()->getVueScript($appId);
+        $this->service = TableService::create(ServicesEnums::VUE->value);
+        $this->service->repository()->getVueScript($appId);
 
         //Определяем источник данных
         foreach ($dataClass->getTableRows() as $row) {
-            $service->repository()->addRow($row);
+            $this->service->repository()->addRow($row);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Метод подключает файлы стилей из директории '/local/App/Infrastructure/Services/Table/VueTable/Assets/styles'
+     * @return $this
+     */
+    public function includeStyles(): self
+    {
+        $this->validateInit();
+
+        $stylesPath = '/include/services/VueTable/styles';
+        $path = Loader::getDocumentRoot() . $stylesPath;
+
+        if (!is_dir(Loader::getDocumentRoot() . $stylesPath)) {
+            throw new RuntimeException('У сервиса не существует стилей!');
+        }
+
+        foreach (array_diff((array)scandir($path), ["..", "."]) as $style) {
+            $this->service->repository()->addStyle($stylesPath . '/' . $style);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param bool $editable
+     * @return self
+     */
+    public function setEditable(bool $editable = true): self
+    {
+        $this->validateInit();
+
+        //Включаем возможность редактирования
+        $this->service->repository()->setEditable($editable);
+
+        return $this;
+    }
+
+    /**
+     * Метод проверяет инициализировано ли Vue-приложение.
+     * Используется перед вызовом методов взаимодействия с ним.
+     * @return void
+     */
+    private function validateInit(): void
+    {
+        if (empty($this->service)) {
+            throw new RuntimeException('Сервис не инициализирован!');
         }
     }
 }
