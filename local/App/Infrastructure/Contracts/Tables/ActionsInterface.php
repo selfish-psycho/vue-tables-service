14f176be76525bae8486ed355e34821b2b890f43
Alexandr Knyazev <asknyazev2402@gmail.com>
2025-02-01 01:02:15 +0500

    Расширил и поправил контроллер Actions

--- local/App/Infrastructure/Contracts/Tables/ActionsInterface.php
+++ local/App/Infrastructure/Contracts/Tables/ActionsInterface.php
@@ -4,11 +4,7 @@ namespace App\Infrastructure\Contracts\Tables;
 
 interface ActionsInterface
 {
-    /**
-     * Метод определяет DOM-узел для инициализации Vue-приложения и сохраняет класс описания сбора данных для таблицы
-     * @param string $appId
-     * @param DataClassInterface $dataClass
-     * @return void
-     */
-    public function init(string $appId, DataClassInterface $dataClass): void;
+    public function init(string $appId, DataClassInterface $dataClass);
+    public function includeStyles(): self;
+    public function setEditable(bool $editable = true): self;
 }
