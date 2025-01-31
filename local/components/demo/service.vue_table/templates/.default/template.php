e0d0b45e3a73b56d8c5bbf67c08430790001b899
Alexandr Knyazev <asknyazev2402@gmail.com>
2025-02-01 01:08:01 +0500

    Упростил template.php новыми методами Actions

--- local/components/demo/service.vue_table/templates/.default/template.php
+++ local/components/demo/service.vue_table/templates/.default/template.php
@@ -3,7 +3,6 @@
 use App\Infrastructure\Enums\Table\DataClassesEnum;
 use App\Infrastructure\Enums\Table\ServicesEnums;
 use App\Infrastructure\Services\Table\TableService;
-use Bitrix\Main\UI\Extension;
 
 /**
  * @var $arResult
@@ -13,14 +12,9 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
     die();
 }
 
-//Подключаем расширение 'BX.Vue'
-Extension::load('ui.vue');
-
-//Создаем пространство для определения Vue-компонента
-$tableDivId = 'app';
 ?>
 
-<div id="<?=$tableDivId?>"></div>
+    <div id="app"></div>
 
 <?php
 
@@ -29,5 +23,8 @@ $dataClass = DataClassesEnum::DEMO->value;
 
 //Инициализируем сервис
 $service = TableService::create(ServicesEnums::VUE->value);
-//Инициализируем Vue-приложение
-$service->actions()->init($tableDivId, (new $dataClass($arResult)));
+$service->actions()
+    ->init('app', (new $dataClass($arResult)))
+    ->includeStyles()
+    ->setEditable()
+;
